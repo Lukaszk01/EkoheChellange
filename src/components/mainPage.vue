4603e910ab85ba822de9c4f04ce87c80e29e75aa
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-05-10 22:05:24 +0900

    main page

---
+++ src/components/mainPage.vue
@@ -0,0 +1,34 @@
+<template>
+
+<h2>yo</h2>
+<div class="show"></div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+.show {
+	opacity: 0.3;
+	background: #D6D8E7;
+	border-radius: 20px;
+	font-family: Roboto-Medium;
+	font-size: 20px;
+	color: rgba(0,0,0,0.87);
+	text-align: left;
+	font-family: Roboto-Medium;
+	font-size: 12px;
+	color: rgba(0,0,0,0.87);
+	text-align: left;
+	opacity: 0.12;
+	background: #D6D8E7;
+	border-radius: 6px;
+	opacity: 0.7;
+}
+
+</style>
